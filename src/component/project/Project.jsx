7ffd23acf676ac7b0103e9ef0f5766229b9fd224
Bhawishya Singh <102045314+Bhawishya509@users.noreply.github.com>
2022-08-21 16:16:36 +0530

    first commit

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -8,8 +8,9 @@ const Project = (props) => {
   const bs4 = new URL("../../img/weather.png", import.meta.url);
   const bs5 = new URL("../../img/todolist.png", import.meta.url);
   const bs6 = new URL("../../img/plotfolio.png", import.meta.url);
-
- 
+  const bs7 = new URL("../../img/education.png", import.meta.url);
+  const bs8 = new URL("../../img/pet.png", import.meta.url);
+  const bs9 = new URL("../../img/food.png", import.meta.url);
   return (
     <>
       {props.nav}
@@ -18,7 +19,7 @@ const Project = (props) => {
         <div
           style={{ height: "0.5px", width: "90%", backgroundColor: "purple" }}
         ></div>
-        <h5 style={{color:"purple"}}>Click me</h5>
+        <h5 style={{ color: "purple" }}>Click me</h5>
         <div className="projectMainBlock">
           <Projectblocks
             val={1}
@@ -27,49 +28,56 @@ const Project = (props) => {
           />
 
           <Projectblocks
-          val={1}
-          link={"https://weatherapi500.netlify.app/"}
-          img={bs4}
-        />
-
-
-
-         
+            val={1}
+            link={"https://educations.vercel.app/"}
+            style={{ height: "100%" }}
+            img={bs7}
+          />
 
           <Projectblocks
             val={1}
-            link={"https://movice.netlify.app/"}
-            img={bs3}
+            style={{ height: "40vh" }}
+            link={"https://wondrous-starship-32658c.netlify.app/"}
+            img={bs8}
           />
-       
 
-          
           <Projectblocks
             val={1}
-            link={"https://patients-sand.vercel.app"}
-            img={bs1}
+            link={"https://food-app-jade-one.vercel.app/"}
+            img={bs9}
           />
-
-              
           <Projectblocks
             val={1}
             link={" https://plotfolio.vercel.app/"}
             img={bs6}
           />
 
+          <Projectblocks
+            val={1}
+            link={"https://patients-sand.vercel.app"}
+            img={bs1}
+          />
 
-           
+          <Projectblocks
+            val={1}
+            link={"https://movice.netlify.app/"}
+            img={bs3}
+          />
+
+          <Projectblocks
+            val={1}
+            link={"https://weatherapi500.netlify.app/"}
+            img={bs4}
+          />
+
           <Projectblocks
             val={1}
             link={"https://tosingh.netlify.app/"}
             img={bs5}
           />
 
-
-
           <Projectblocks val={0} />
 
-       
           <Projectblocks val={0} />
 
           <Projectblocks val={1} />
